dbb2466300af30538c8d5f37b5fe49d65c9938a1
Sébastien Doubey <sebastien.doubey@gmail.com>
2019-07-06 06:06:02 +0200

    Add empty DomainEntity @StaticMetamodel

---
+++ src/main/java/eu/lycoris/spring/ddd/domain/DomainEntity_.java
@@ -0,0 +1,8 @@
+package eu.lycoris.spring.ddd.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(DomainEntity.class)
+public class DomainEntity_ {}
