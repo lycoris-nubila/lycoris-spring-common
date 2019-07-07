090b73cfdc485747c34334ada1ac311574830a5c
Sébastien Doubey <sebastien.doubey@gmail.com>
2019-07-07 18:26:34 +0200

    Update DomainEntity @StaticMetamodel

--- src/main/java/eu/lycoris/spring/ddd/domain/DomainEntity_.java
+++ src/main/java/eu/lycoris/spring/ddd/domain/DomainEntity_.java
@@ -1,8 +1,20 @@
 package eu.lycoris.spring.ddd.domain;
 
+import java.time.Instant;
+
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(DomainEntity.class)
-public class DomainEntity_ {}
+public class DomainEntity_ {
+
+  public static volatile SingularAttribute<DomainEntity<?>, ?> id;
+  public static volatile SingularAttribute<DomainEntity<?>, Instant> updateDateTime;
+  public static volatile SingularAttribute<DomainEntity<?>, Instant> creationDateTime;
+
+  public static final String ID = "id";
+  public static final String UPDATE_DATE_TIME = "updateDateTime";
+  public static final String CREATION_DATE_TIME = "creationDateTime";
+}
