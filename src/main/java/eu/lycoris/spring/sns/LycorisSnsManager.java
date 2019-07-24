2b421ed9c7d0d296148eb4dab02b4efc905b5cff
Sébastien Doubey <sebastien.doubey@gmail.com>
2019-07-24 16:07:10 +0200

    Add handleMessage in LycorisSnsManager

--- src/main/java/eu/lycoris/spring/sns/LycorisSnsManager.java
+++ src/main/java/eu/lycoris/spring/sns/LycorisSnsManager.java
@@ -1,10 +1,14 @@
 package eu.lycoris.spring.sns;
 
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.message.SnsMessageHandler;
 import com.amazonaws.services.sns.message.SnsMessageManager;
 
 import eu.lycoris.spring.common.LycorisSubjectMessage;
@@ -23,5 +27,9 @@ public class LycorisSnsManager extends SnsMessageManager {
 
   public void sendMessage(String snsTopic, LycorisSubjectMessage message) {
     notifier.sendNotification(snsTopic, message, message.getSubject());
+  }
+
+  public void handleMessage(String messageBody, SnsMessageHandler handler) {
+    parseMessage(IOUtils.toInputStream(messageBody, StandardCharsets.UTF_8)).handle(handler);
   }
 }
