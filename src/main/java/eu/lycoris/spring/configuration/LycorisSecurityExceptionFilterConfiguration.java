3ec07171bd8f0f2c644a6aae6c3e8b82c55b8b8e
ztahari <zeineb.tahari@gmail.com>
2021-12-08 16:52:48 +0100

    Mettre en place le filter firewall de spring dans spring-common

---
+++ src/main/java/eu/lycoris/spring/configuration/LycorisSecurityExceptionFilterConfiguration.java
@@ -0,0 +1,8 @@
+package eu.lycoris.spring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("eu.lycoris.spring.security")
+public class LycorisSecurityExceptionFilterConfiguration {}
