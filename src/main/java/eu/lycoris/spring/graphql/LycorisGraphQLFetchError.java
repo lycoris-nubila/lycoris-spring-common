7c195620bdbda785a4529edd60b42e45a7acc3ae
sdoubey <sebastien.doubey@gmail.com>
2022-03-21 17:08:00 +0100

    Handle UndeclaredThrowableException

--- src/main/java/eu/lycoris/spring/graphql/LycorisGraphQLFetchError.java
+++ src/main/java/eu/lycoris/spring/graphql/LycorisGraphQLFetchError.java
@@ -30,103 +30,108 @@ import static graphql.Assert.assertNotNull;
 @SuppressWarnings("serial")
 public class LycorisGraphQLFetchError implements GraphQLError {
 
-    private final String message;
-    private final transient List<Object> path;
-    private final Throwable exception;
-    private final List<SourceLocation> locations;
-    private final transient Map<String, Object> extensions;
+  private final String message;
+  private final transient List<Object> path;
+  private final Throwable exception;
+  private final List<SourceLocation> locations;
+  private final transient Map<String, Object> extensions;
 
-    public LycorisGraphQLFetchError(
-            MessageSource messageSource,
-            ResultPath path,
-            Throwable exception,
-            SourceLocation sourceLocation) {
-        this.path = assertNotNull(path).toList();
-        this.exception = assertNotNull(exception);
-        this.locations = Collections.singletonList(sourceLocation);
-        this.extensions = mkExtensions(exception);
-        this.message = mkMessage(exception, messageSource);
+  public LycorisGraphQLFetchError(
+      MessageSource messageSource,
+      ResultPath path,
+      Throwable exception,
+      SourceLocation sourceLocation) {
+    this.path = assertNotNull(path).toList();
+    this.exception = assertNotNull(exception);
+    this.locations = Collections.singletonList(sourceLocation);
+    this.extensions = mkExtensions(exception);
+    this.message = mkMessage(exception, messageSource);
+  }
+
+  private String mkMessage(Throwable exception, MessageSource messageSource) {
+    int violationExceptionIndex =
+        ExceptionUtils.indexOfThrowable(exception, ConstraintViolationException.class);
+    if (violationExceptionIndex >= 0) {
+      ConstraintViolationException violationException =
+          (ConstraintViolationException)
+              ExceptionUtils.getThrowables(exception)[violationExceptionIndex];
+
+      String fields =
+          violationException.getConstraintViolations().stream()
+              .map(ConstraintViolation::getPropertyPath)
+              .map(Path::toString)
+              .collect(Collectors.joining(", "));
+
+      return messageSource.getMessage(
+          ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION,
+          new Object[] {fields},
+          ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION,
+          LocaleContextHolder.getLocale());
+    } else if (exception instanceof UndeclaredThrowableException) {
+      UndeclaredThrowableException ute = ((UndeclaredThrowableException) exception);
+      return messageSource.getMessage(
+          ute.getUndeclaredThrowable().getMessage(),
+          new Object[] {},
+          ute.getUndeclaredThrowable().getMessage(),
+          LocaleContextHolder.getLocale());
+    } else if (exception instanceof CompletionException) {
+      return messageSource.getMessage(
+          exception.getCause().getMessage(),
+          new Object[] {},
+          exception.getCause().getMessage(),
+          LocaleContextHolder.getLocale());
+    } else {
+      return messageSource.getMessage(
+          exception.getMessage(),
+          new Object[] {},
+          exception.getMessage(),
+          LocaleContextHolder.getLocale());
+    }
+  }
+
+  private Map<String, Object> mkExtensions(Throwable exception) {
+    Map<String, Object> ext = new HashMap<>();
+
+    if (exception instanceof UndeclaredThrowableException) {
+      exception = ((UndeclaredThrowableException) exception).getUndeclaredThrowable();
+    } else if (exception instanceof CompletionException) {
+      exception = exception.getCause();
     }
 
-    private String mkMessage(Throwable exception, MessageSource messageSource) {
-        int violationExceptionIndex =
-                ExceptionUtils.indexOfThrowable(exception, ConstraintViolationException.class);
-        if (violationExceptionIndex >= 0) {
-            ConstraintViolationException violationException =
-                    (ConstraintViolationException)
-                            ExceptionUtils.getThrowables(exception)[violationExceptionIndex];
-
-            String fields =
-                    violationException
-                            .getConstraintViolations()
-                            .stream()
-                            .map(ConstraintViolation::getPropertyPath)
-                            .map(Path::toString)
-                            .collect(Collectors.joining(", "));
-
-            return messageSource.getMessage(
-                    ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION,
-                    new Object[]{fields},
-                    ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION,
-                    LocaleContextHolder.getLocale());
-        } else if (exception instanceof UndeclaredThrowableException) {
-            UndeclaredThrowableException ute = ((UndeclaredThrowableException) exception);
-            return messageSource.getMessage(
-                    ute.getUndeclaredThrowable().getMessage(),
-                    new Object[]{},
-                    ute.getUndeclaredThrowable().getMessage(),
-                    LocaleContextHolder.getLocale());
-        } else if (exception instanceof CompletionException) {
-            return messageSource.getMessage(
-                    exception.getCause().getMessage(),
-                    new Object[]{},
-                    exception.getCause().getMessage(),
-                    LocaleContextHolder.getLocale());
-        } else {
-            return messageSource.getMessage(
-                    exception.getMessage(),
-                    new Object[]{},
-                    exception.getMessage(),
-                    LocaleContextHolder.getLocale());
-        }
+    if (exception instanceof LycorisAuthenticationException) {
+      ext.put("type", "AUTHENTICATION");
+    } else {
+      ext.put("type", "GENERAL");
     }
+    return ext;
+  }
 
-    private Map<String, Object> mkExtensions(Throwable exception) {
-        Map<String, Object> ext = new HashMap<>();
-        if (exception instanceof LycorisAuthenticationException) {
-            ext.put("type", "AUTHENTICATION");
-        } else {
-            ext.put("type", "GENERAL");
-        }
-        return ext;
-    }
+  public Throwable getException() {
+    return exception;
+  }
 
-    public Throwable getException() {
-        return exception;
-    }
+  @Override
+  public String getMessage() {
+    return message;
+  }
 
-    @Override
-    public String getMessage() {
-        return message;
-    }
+  @Override
+  public List<SourceLocation> getLocations() {
+    return locations;
+  }
 
-    @Override
-    public List<SourceLocation> getLocations() {
-        return locations;
-    }
+  @Override
+  public List<Object> getPath() {
+    return path;
+  }
 
-    @Override
-    public List<Object> getPath() {
-        return path;
-    }
+  @Override
+  public Map<String, Object> getExtensions() {
+    return extensions;
+  }
 
-    @Override
-    public Map<String, Object> getExtensions() {
-        return extensions;
-    }
-
-    @Override
-    public ErrorType getErrorType() {
-        return ErrorType.DataFetchingException;
-    }
+  @Override
+  public ErrorType getErrorType() {
+    return ErrorType.DataFetchingException;
+  }
 }
