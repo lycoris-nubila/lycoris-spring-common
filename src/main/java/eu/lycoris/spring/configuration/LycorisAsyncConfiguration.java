5226ea22938fcbbe9f162b875153e152fafcfb14
sdoubey <sebastien.doubey@gmail.com>
2022-04-27 10:40:47 +0200

    Make threadPoolTaskExecutor primary

--- src/main/java/eu/lycoris/spring/configuration/LycorisAsyncConfiguration.java
+++ src/main/java/eu/lycoris/spring/configuration/LycorisAsyncConfiguration.java
@@ -2,6 +2,7 @@ package eu.lycoris.spring.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 public class LycorisAsyncConfiguration {
 
   @Bean
+  @Primary
   public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
     executor.setWaitForTasksToCompleteOnShutdown(true);
