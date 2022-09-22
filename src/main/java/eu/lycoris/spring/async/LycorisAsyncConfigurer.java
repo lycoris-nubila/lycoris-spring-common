3324f210037d9561840438e8a411bf3738a16876
sdoubey <it@lycoris.eu>
2022-09-22 11:58:10 +0200

    Add Nullability and update Domain/VO event

--- src/main/java/eu/lycoris/spring/async/LycorisAsyncConfigurer.java
+++ src/main/java/eu/lycoris/spring/async/LycorisAsyncConfigurer.java
@@ -6,30 +6,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import javax.validation.constraints.NotNull;
+import javax.annotation.Nullable;
 import java.util.concurrent.Executor;
 
 @Configuration
 public class LycorisAsyncConfigurer implements AsyncConfigurer {
 
-  private final @NotNull ThreadPoolTaskExecutor taskExecutor;
+    private final @Nullable ThreadPoolTaskExecutor taskExecutor;
 
-  private final @NotNull AsyncUncaughtExceptionHandler exceptionHandler;
+    private final @Nullable AsyncUncaughtExceptionHandler exceptionHandler;
 
-  public LycorisAsyncConfigurer(
-      @NotNull ThreadPoolTaskExecutor taskExecutor,
-      @NotNull AsyncUncaughtExceptionHandler exceptionHandler) {
-    this.taskExecutor = taskExecutor;
-    this.exceptionHandler = exceptionHandler;
-  }
+    public LycorisAsyncConfigurer(
+            @Nullable @Autowired(required = false) ThreadPoolTaskExecutor taskExecutor,
+            @Nullable @Autowired(required = false)
+            AsyncUncaughtExceptionHandler exceptionHandler) {
+        this.taskExecutor = taskExecutor;
+        this.exceptionHandler = exceptionHandler;
+    }
 
-  @Override
-  public Executor getAsyncExecutor() {
-    return this.taskExecutor;
-  }
+    @Override
+    @Nullable
+    public Executor getAsyncExecutor() {
+        return this.taskExecutor;
+    }
 
-  @Override
-  public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-    return this.exceptionHandler;
-  }
+    @Override
+    @Nullable
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return this.exceptionHandler;
+    }
 }
