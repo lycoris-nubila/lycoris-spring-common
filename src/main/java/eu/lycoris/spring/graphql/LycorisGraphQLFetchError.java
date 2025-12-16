af43adcf1b746035c8ac82a3c1e354e0b2f8a05a
sdoubey <sebastien@remembr.net>
2025-12-16 15:19:23 +0100

    Handle some graphql exceptions

--- src/main/java/eu/lycoris/spring/graphql/LycorisGraphQLFetchError.java
+++ src/main/java/eu/lycoris/spring/graphql/LycorisGraphQLFetchError.java
@@ -3,17 +3,19 @@ package eu.lycoris.spring.graphql;
 import static eu.lycoris.spring.common.LycorisMessages.ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION;
 import static graphql.Assert.assertNotNull;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletionException;
 import java.util.stream.Collectors;
 
+import javax.persistence.PersistenceException;
+import javax.persistence.RollbackException;
+import javax.transaction.TransactionalException;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Path;
 
+import eu.lycoris.spring.common.LycorisApplicationException;
+import lombok.Getter;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -25,102 +27,111 @@ import graphql.execution.ResultPath;
 import graphql.language.SourceLocation;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.springframework.dao.DataAccessException;
+import org.springframework.transaction.TransactionException;
 
 @ToString
 @EqualsAndHashCode
 @SuppressWarnings("serial")
 public class LycorisGraphQLFetchError implements GraphQLError {
 
-  private final String message;
-  private final transient List<Object> path;
-  private final Throwable exception;
-  private final List<SourceLocation> locations;
-  private final transient Map<String, Object> extensions;
+    private final String message;
+    private final transient List<Object> path;
+    @Getter
+    private final Throwable exception;
+    private final List<SourceLocation> locations;
+    private final transient Map<String, Object> extensions;
 
-  public LycorisGraphQLFetchError(
-      MessageSource messageSource,
-      ResultPath path,
-      Throwable exception,
-      SourceLocation sourceLocation) {
-    this.path = assertNotNull(path).toList();
-    this.exception = assertNotNull(exception);
-    this.locations = Collections.singletonList(sourceLocation);
-    this.extensions = mkExtensions(exception);
-    this.message = mkMessage(exception, messageSource);
-  }
-
-  private String mkMessage(Throwable exception, MessageSource messageSource) {
-    int violationExceptionIndex =
-        ExceptionUtils.indexOfThrowable(exception, ConstraintViolationException.class);
-    if (violationExceptionIndex >= 0) {
-      ConstraintViolationException violationException =
-          (ConstraintViolationException)
-              ExceptionUtils.getThrowables(exception)[violationExceptionIndex];
-
-      String fields =
-          violationException
-              .getConstraintViolations()
-              .stream()
-              .map(ConstraintViolation::getPropertyPath)
-              .map(Path::toString)
-              .collect(Collectors.joining(", "));
-
-      return messageSource.getMessage(
-          ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION,
-          new Object[] {fields},
-          ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION,
-          LocaleContextHolder.getLocale());
-    } else if (exception instanceof CompletionException) {
-      return messageSource.getMessage(
-          exception.getCause().getMessage(),
-          new Object[] {},
-          exception.getCause().getMessage(),
-          LocaleContextHolder.getLocale());
-    } else {
-      return messageSource.getMessage(
-          exception.getMessage(),
-          new Object[] {},
-          exception.getMessage(),
-          LocaleContextHolder.getLocale());
+    public LycorisGraphQLFetchError(
+            MessageSource messageSource,
+            ResultPath path,
+            Throwable exception,
+            SourceLocation sourceLocation) {
+        this.path = assertNotNull(path).toList();
+        this.exception = assertNotNull(exception);
+        this.locations = Collections.singletonList(sourceLocation);
+        this.extensions = mkExtensions(exception);
+        this.message = mkMessage(exception, messageSource);
     }
-  }
 
-  private Map<String, Object> mkExtensions(Throwable exception) {
-    Map<String, Object> ext = new HashMap<>();
-    if (exception instanceof LycorisAuthenticationException) {
-      ext.put("type", "AUTHENTICATION");
-    } else {
-      ext.put("type", "GENERAL");
+    private String mkMessage(Throwable exception, MessageSource messageSource) {
+        int violationExceptionIndex =
+                ExceptionUtils.indexOfThrowable(exception, ConstraintViolationException.class);
+        if (violationExceptionIndex >= 0) {
+            ConstraintViolationException violationException =
+                    (ConstraintViolationException)
+                            ExceptionUtils.getThrowables(exception)[violationExceptionIndex];
+
+            String fields =
+                    violationException
+                            .getConstraintViolations()
+                            .stream()
+                            .map(ConstraintViolation::getPropertyPath)
+                            .map(Path::toString)
+                            .collect(Collectors.joining(", "));
+
+            return messageSource.getMessage(
+                    ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION,
+                    new Object[]{fields},
+                    ERROR_WEB_REQUEST_CONSTRAINT_VIOLATION,
+                    LocaleContextHolder.getLocale());
+        } else if (exception instanceof CompletionException) {
+            return messageSource.getMessage(
+                    exception.getCause().getMessage(),
+                    new Object[]{},
+                    exception.getCause().getMessage(),
+                    LocaleContextHolder.getLocale());
+        } else {
+            Throwable throwable = Arrays.stream(ExceptionUtils.getThrowables(exception))
+                    .filter(e -> Arrays.asList(
+                            LycorisAuthenticationException.class,
+                            LycorisApplicationException.class,
+                            PersistenceException.class,
+                            TransactionException.class,
+                            DataAccessException.class
+                    ).contains(e.getClass()) && !(e instanceof RollbackException)).
+                    findFirst().orElse(exception);
+
+            return messageSource.getMessage(
+                    throwable.getMessage(),
+                    new Object[]{},
+                    throwable.getMessage(),
+                    LocaleContextHolder.getLocale());
+        }
     }
-    return ext;
-  }
 
-  public Throwable getException() {
-    return exception;
-  }
+    private Map<String, Object> mkExtensions(Throwable exception) {
+        Map<String, Object> ext = new HashMap<>();
+        if (exception instanceof LycorisAuthenticationException) {
+            ext.put("type", "AUTHENTICATION");
+        } else {
+            ext.put("type", "GENERAL");
+        }
+        return ext;
+    }
 
-  @Override
-  public String getMessage() {
-    return message;
-  }
+    @Override
+    public String getMessage() {
+        return message;
+    }
 
-  @Override
-  public List<SourceLocation> getLocations() {
-    return locations;
-  }
+    @Override
+    public List<SourceLocation> getLocations() {
+        return locations;
+    }
 
-  @Override
-  public List<Object> getPath() {
-    return path;
-  }
+    @Override
+    public List<Object> getPath() {
+        return path;
+    }
 
-  @Override
-  public Map<String, Object> getExtensions() {
-    return extensions;
-  }
+    @Override
+    public Map<String, Object> getExtensions() {
+        return extensions;
+    }
 
-  @Override
-  public ErrorType getErrorType() {
-    return ErrorType.DataFetchingException;
-  }
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.DataFetchingException;
+    }
 }
